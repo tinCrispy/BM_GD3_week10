1ccbedc1afaf41a4122a6990024d6d2ef07e832f
tinCrispy <basilmarples@outlook.com>
2024-09-30 11:51:07 +0100

    BM_GD3_week10_lab1
    Lazerrrrr

---
+++ BM_GD3_week10/Assets/TutorialInfo/Scripts/RaycastShoot.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class ShootWithRaycast : MonoBehaviour
+{
+    public int damage = 1;
+    public float fireRate = 0.2f;
+    public float weaponRange = 50;
+    public float hitForce = 100;
+    public Transform gunEnd;
+
+    private Camera fpsCam;
+    private WaitForSeconds shotDuration = new WaitForSeconds(0.07f);
+    private AudioSource gunAudio;
+    private LineRenderer laserLine;
+    private float nextFire;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        laserLine = GetComponent<LineRenderer>();
+        gunAudio = GetComponent<AudioSource>();
+        fpsCam = GetComponentInParent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && Time.time > nextFire)
+        {
+
+            nextFire = Time.time + fireRate;
+            StartCoroutine(ShotEffect());
+        }
+
+        Vector3 rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+
+        RaycastHit hit;
+
+        laserLine.SetPosition(0, gunEnd.position);
+
+        if (Physics.Raycast(rayOrigin, fpsCam.transform.forward, out hit, weaponRange))
+        {
+            laserLine.SetPosition(1, hit.point);
+        }
+
+        else
+        {
+            laserLine.SetPosition(1, fpsCam.transform.forward * weaponRange);
+        }
+
+    }
+
+    private IEnumerator ShotEffect()
+    {
+        gunAudio.Play();
+        laserLine.enabled = true;
+        yield return shotDuration;
+        laserLine.enabled = false;
+    }
+}
